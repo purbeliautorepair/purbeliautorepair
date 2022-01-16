b28bfd154766664591b348cd662d74e6ff25a66b
Raj Pokhrel <rajpokhrel@Rajs-MacBook-Pro.local>
2022-01-15 19:53:17 -0500

    test

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@
 (function($){
 	$(document).ready(function(){
 	
-		$(".banner-image").backstretch('images/banner.jpg');
+		$(".banner-image").backstretch('images/profile1.png');
 		
 		// Fixed header
 		//-----------------------------------------------
